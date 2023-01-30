f0672166c3c4a16c702d1711dd5ed83aa107f948
bulfinch222 <123668850+bulfinch222@users.noreply.github.com>
2023-01-30 21:16:58 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,19 +1,14 @@
 const { VK } = require('vk-io');  
- 
 const vk = new VK({
 token: 'vk1.a.5WQNVBapNRgr1fzlxUZL8Bs3pTlRsWD5fNGftx0yRriMyDPlI8FRUGMadl4y343qdSLUFWx0DbyYcrOcs7kyZuoNIVTqWu5lURzH2DVujaN2Z0hiUoUppMjO4XDapvu8BcTj2K9dmmWtae-qLji_iyuz4Iq-jzstMEwIFZ2BpbfUY3Pr5X9BvqOJXUdpwSEZJbYYnmOxtJ3vC5gMNU40TQ'
 });
 console.log("By Tripl_color , awoken.xyz".info);
 function startamuu() {
 vk.api.wall.createComment({
-owner_id: 628544571,
-post_id: 129,
+owner_id: -174105461,
+post_id: 6713149,
 message: 'ферма'
 });
 console.log("Коментарий сделан успешно".success);
 }
-
-setInterval(startamuu, 14520000);               
-
-                                                           
-
+setInterval(startamuu, 300000); 
